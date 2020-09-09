fdb55a746a33190a35e27b4dadd5ae1c15e7c232
Rasheed Starlet <starletgh@gmail.com>
2020-09-09 20:04:27 +0000

    Configured templates and static files

--- hl/settings.py
+++ hl/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'hl.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / 'templates' ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,3 +124,5 @@ AUTH_USER_MODEL = 'users.User'
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [ BASE_DIR / 'static' ]
