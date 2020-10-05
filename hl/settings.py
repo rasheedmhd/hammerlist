36fd040f8e8b623b916f92ff3cd331d90bd90ae6
Rasheed Starlet <starletgh@gmail.com>
2020-10-05 10:03:06 +0000

    Updated dj-database-url config in settings

--- hl/settings.py
+++ hl/settings.py
@@ -151,4 +151,5 @@ STATICFILES_DIRS = [ BASE_DIR / 'static' ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=500, ssl_require=True)
+db_from_env = dj_database_url.config(conn_max_age=500, ssl_require=True)
+DATABASES['default'].update(db_from_env)
