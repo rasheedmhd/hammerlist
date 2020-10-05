3bfa022fdd0cc666f72e2c1597d553a551650b00
Rasheed Starlet <starletgh@gmail.com>
2020-10-05 17:52:54 +0000

    Added Category Feature

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,7 @@ from .views import (
 	category,
 	furnitures,
 	furniture_detail,
+	AdCategory,
 )
 
 app_name = "home"
@@ -12,7 +13,8 @@ app_name = "home"
 urlpatterns = [
 	path('',  home, name="home"),
 	path('ads/join/', ads, name="ads"),
-	path('furniture/categories/<str:slug>/', category, name="category"),
+	path('furniture/categories/', category, name="category"),
+	path('furniture/categories/<str:slug>/', AdCategory.as_view(), name="ad_by_category"),
 	path('furniture/', furnitures, name="furniture"),
 	path('furniture/details/<int:pk>/', furniture_detail.as_view(), name="furniture_detail"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
+from django.views.generic.list import ListView
 
 from .models import Furniture, Category
 
 # Create your views here.
 def home(request):
-	return render(request, 'main/homepage.html')
+	categories = Category.objects.all()
+	return render(request, 'main/homepage.html', { 'categories': categories })
 
 def ads(request):
 	return render(request, 'main/ads.html')
@@ -13,6 +15,19 @@ def ads(request):
 def category(request):
 	categories = Category.objects.all()
 	return render(request, 'main/furniture/category.html', { 'categories': categories })
+
+class AdCategory(ListView):
+    model = Furniture
+    template_name = 'main/furniture/ad_by_category.html'
+
+    def get_queryset(self):
+        self.category = get_object_or_404(Category, slug=self.kwargs['slug'])
+        return Furniture.objects.filter(category=self.category)
+
+    def get_context_data(self, **kwargs):
+        context = super(AdCategory, self).get_context_data(**kwargs)
+        context['category'] = self.category
+        return context
 
 def furnitures(request):
 	furnitures = Furniture.objects.all()
